046902b925de1f0e2c724260b2918ea1478ea52b
Joel Wanner <wannerjo@student.ethz.ch>
2017-06-03 23:33:58 +0200

    Add optimized parameters to SMT solver

--- src/smt/solver.py
+++ src/smt/solver.py
@@ -12,6 +12,18 @@ class SmtSolver(object):
     def solve(self, assertions):
         s = Solver()
 
+        # Parameters
+        # -----------------------------
+        s.set('smt.ematching', False)     # Disable E-matching based quantifier instantiation
+        s.set('smt.phase_selection', 0)   # Phase selection heuristic: always false
+        s.set('smt.restart_strategy', 0)  # Restart strategy: geometric
+        s.set('smt.restart_factor', 1.5)  # Increase restart threshold multiplication factor (geometric progression)
+        s.set('smt.arith.random_initial_value', True)  # Use random initial values in procedure for linear arithmetic
+        s.set('smt.case_split', 3)        # Case split based on relevancy (structural splitting)
+        s.set('smt.delay_units', True)    # Prevent Z3 from restarting when a unit clause is learned
+        s.set('sk_hack', True)            # Enable hack for VCC
+        s.set('smt.qi.eager_threshold', 100.0)  # Increase threshold for eager quantifier instantiation (default: 10.0)
+
         for a in assertions:
             s.add(a)
 
